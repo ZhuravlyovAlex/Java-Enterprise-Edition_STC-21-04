9eedf777243f4a9ff2f52dde94938569cda1df1e
Alexey <ZhuravlyovAlex@mail.ru>
2021-03-28 10:47:18 +0300

    new lesson 07 task_bonus

---
+++ src/main/java/lesson07/task_bonus/TextConverter.java
@@ -0,0 +1,46 @@
+package lesson07.task_bonus;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * программа, которая читающая файл в кодировке UTF-8, а пишет в другой файл в кодировке UTF-16.
+ * В программе используется InputStreamReader/OutputStreamWriter
+ * * <p>
+ * version 1.0
+ * Copyright Журавлёв Алексей
+ */
+
+public class TextConverter {
+
+    public static void main(String[] args) {
+        File directory = new File("textConverter");
+        directory.mkdir();
+        File fileUtf8 = new File(directory, "fileUtf8.txt");
+        File fileUtf16 = new File(directory, "fileUtf16.txt");
+
+        try {
+            fileUtf8.createNewFile();
+            fileUtf16.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        StringBuilder result = new StringBuilder();
+        try (Reader reader = new InputStreamReader(new FileInputStream(fileUtf8))) {
+            int a = reader.read();
+            while (a > 0) {
+                result.append((char) a);
+                a = reader.read();
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(fileUtf16), StandardCharsets.UTF_16)) {
+            writer.write(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
